a5e8c42439a9a9137e9ebc239d8bace461fbf1b4
dikomako <43717646+dikomako@users.noreply.github.com>
2018-11-01 19:27:01 +0100

    projekt létrehozása
    A weboldalon leírtak szerint generáltam egy projektet. Beletettem az alap dependenciákat, amik az órai anyagban is benne vannak. Ezt majd utólag úgyis tudjuk módosítani.

---
+++ WarehouseManager/src/main/java/hu/elte/WarehouseManager/WarehouseManagerApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.WarehouseManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManagerApplication.class, args);
+	}
+}
